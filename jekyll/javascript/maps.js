6c8022b6584b815cf90fa851a8667b029faa8737
Mike Raynham <mike.raynham@sitpub.com>
2026-04-11 06:19:25 +0100

    Remove Substation

--- jekyll/javascript/maps.js
+++ jekyll/javascript/maps.js
@@ -1,44 +1,25 @@
 async function initMap() {
-  loadMap({
-    elementId: 'gawsworth-map',
-    center: { lat: 53.2305637, lng: -2.168794 },
-    infoContent: `
-      <div><strong>Wild About Pilates</strong><br>
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  const elem = document.getElementById('gawsworth-map');
+  if (elem === null) return;
+
+  const center = { lat: 53.2305637, lng: -2.168794 };
+
+  const map = new Map(elem, {
+    zoom: 15,
+    center: center,
+    mapId: '696202b44bbd260d4ae016f2',
+  });
+
+  const infowindow = new google.maps.InfoWindow({
+    content: `<div><strong>Wild About Pilates</strong><br>
       Gawsworth Village Hall<br>
       Church Lane<br>
       Macclesfield<br>
       SK11 9RJ</div>`
   });
-
-  loadMap({
-    elementId: 'substation-map',
-    center: { lat: 53.268268, lng: -2.118140 },
-    infoContent: `
-      <div><strong>Wild About Pilates</strong><br>
-      Substation<br>
-      Queen&lsquo;s Avenue<br>
-      Macclesfield<br>
-      SK10 2BN</div>`
-  });
-}
-
-async function loadMap({ elementId, center, infoContent }) {
-  const elem = document.getElementById(elementId);
-  if (elem === null) return;
-
-  const { Map } = await google.maps.importLibrary("maps");
-
-  const map = new Map(elem, {
-    zoom: 15,
-    center: center,
-    mapId: elementId,
-  });
-
-  const infowindow = new google.maps.InfoWindow({
-    content: infoContent
-  });
-
-  const {AdvancedMarkerElement} = await google.maps.importLibrary("marker");
 
   const marker = new AdvancedMarkerElement({
     map: map,
